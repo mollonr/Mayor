a1cd50474e8e006eab906f80cf8bfa1cc583a786
mollonr <mollonr@gmail.com>
2017-04-11 23:14:52 -0300

    Update Mayor.cpp

--- Mayor.cpp
+++ Mayor.cpp
@@ -8,19 +8,23 @@ V 20170410.0
 
 int main ()
 {
-int a,b,c;
-a=0; b=0; c=0;
+int a,b;
+a=0; b=0;
 std :: cout << "Ingrese el primer valor a comparar: \n";
 std :: cin >> a;
 std :: cout << "Ingrese el segundo valor a comparar: \n";
 std :: cin >> b;
 if (a == b)
 	std :: cout << "Ambos numeros son iguales. \n";
+else if (a>b)
+{
+    std :: cout << "El mayor valor de los dos es: \n";
+    std :: cout << a;
+}
 else
 {
-    c = (a>b) ? a : b;
     std :: cout << "El mayor valor de los dos es: \n";
-    std :: cout << c;
+    std :: cout << b;
 }
 
 }
